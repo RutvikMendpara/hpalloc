9576cd3fd62d0ddfdd3025e13f7fc4d278fecaa1
Rutvik Mendpara <rutvikm200@gmail.com>
2025-07-01 01:28:53 +0000

    create and write SizeClassAllocator core

--- include/hpalloc/size_class.h
+++ include/hpalloc/size_class.h
@@ -1,1 +1,26 @@
 #pragma once
+
+
+#include "hpalloc/fixed_block.h"
+#include "hpalloc/arena.h"
+
+#include <array>
+#include <cstddef>
+#include <memory>
+#include <unordered_map>
+
+inline constexpr size_t kSizeClasses[] = {32, 64, 128, 256};
+inline constexpr size_t kNumClasses = std::size(kSizeClasses);
+
+class SizeClassAllocator {
+public:
+    explicit SizeClassAllocator(size_t arena_size_per_class = 1 << 20); // 1MB by default
+    void* allocate(size_t size, size_t& actual_class_size);
+    void deallocate(void* ptr, size_t actual_class_size);
+
+private:
+    std::unordered_map<size_t, Fixed_Block> allocators_;
+    std::array<std::unique_ptr<Arena>, kNumClasses> arenas_;
+
+};
+
--- src/size_class.cpp
+++ src/size_class.cpp
@@ -1,1 +1,30 @@
 #include "hpalloc/size_class.h"
+
+SizeClassAllocator::SizeClassAllocator(size_t arena_size_per_class) {
+    for (size_t i = 0; i < kNumClasses; ++i) {
+        size_t size_class = kSizeClasses[i];
+        auto arena = std::make_unique<Arena>(arena_size_per_class);
+
+        allocators_.emplace(size_class, Fixed_Block(*arena, size_class));
+        arenas_[i] = std::move(arena);
+    }
+}
+
+void* SizeClassAllocator::allocate(size_t size, size_t& actual_class_size) {
+    for (size_t sc : kSizeClasses) {
+        if (size <= sc) {
+            actual_class_size = sc;
+            return allocators_.at(sc).allocate();
+        }
+    }
+    throw std::invalid_argument("Unsupported allocation size");
+}
+
+void SizeClassAllocator::deallocate(void* ptr, size_t actual_class_size) {
+    auto it = allocators_.find(actual_class_size);
+    if (it == allocators_.end()) {
+        throw std::invalid_argument("Invalid size class passed to deallocate");
+    }
+    it->second.deallocate(ptr);
+}
+
