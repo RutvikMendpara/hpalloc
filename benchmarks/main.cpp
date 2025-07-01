3749c69b457f7e9babf6fca2263dc17278d13162
Rutvik Mendpara <rutvikm200@gmail.com>
2025-07-01 02:16:17 +0000

    add size_class_benchmark

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -1,1 +1,35 @@
+#include <chrono>
+#include <iostream>
+#include <vector>
 #include "hpalloc/allocator.h"
+
+void run_size_class_benchmark();
+int main() {
+    run_size_class_benchmark();
+    return 0;
+}
+
+void run_size_class_benchmark() {
+    constexpr size_t N = 10'000'000;
+
+    for (size_t size : kSizeClasses) {
+        SizeClassAllocator allocator;
+        std::vector<void*> blocks;
+        blocks.reserve(N);
+
+        size_t actual_class;
+        auto start = std::chrono::high_resolution_clock::now();
+
+        for (size_t i = 0; i < N; ++i)
+            blocks.push_back(allocator.allocate(size, actual_class));
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+
+        std::cout << "[Perf] Allocated " << N << " blocks of size " << size
+                  << " in " << duration.count() << " sec\n";
+
+        for (void* ptr : blocks)
+            allocator.deallocate(ptr, actual_class);
+    }
+}
